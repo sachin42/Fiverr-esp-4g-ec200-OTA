27153abab9bd393911e411b860407821692743a2
Sachin Kushawaha <54734235+sachin42@users.noreply.github.com>
2025-02-10 16:41:48 +0530

    Refactor OTA update process by extracting server URL components and improving error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #define TINY_GSM_MODEM_EC200U
-// #define TINY_GSM_MODEM_A7670
 
 #define MODEM_TX_PIN (17)
 #define MODEM_RX_PIN (16)
@@ -11,8 +10,11 @@
 
 #include <TinyGsmClient.h>
 #include <Update.h>
+#include <ArduinoHttpClient.h>
 
-const char *server_url = "http://protocol.electrocus.com:8000/firmware.bin";
+const char *server_url = "protocol.electrocus.com"; // Extract host from URL
+const int server_port = 8000;
+const char *firmware_path = "/firmware.bin"; // Extract file path
 
 #ifdef DUMP_AT_COMMANDS // if enabled it requires the streamDebugger lib
 #include <StreamDebugger.h>
@@ -32,7 +34,6 @@ TaskHandle_t ota_task_handle = NULL;
 void blink_task(void *pv)
 {
     pinMode(2, OUTPUT);
-
     while (true)
     {
         digitalWrite(2, HIGH);
@@ -47,26 +48,20 @@ static void __attribute__((noreturn)) task_fatal_error(void)
 {
     Serial.println("Exiting task due to fatal error...");
     ota_task_handle = NULL;
-    (void)vTaskDelete(NULL);
-
-    while (1)
-    {
-        ;
-    }
+    vTaskDelete(NULL);
+    while (1) {}
 }
 
 bool isFirmwareUpdateAvailable(const String &header, const String &currentVersion)
 {
     String newVersion = "";
-
-    // Find "X-Firmware-Version: " in the header
     int index = header.indexOf("X-Firmware-Version: ");
     if (index != -1)
     {
-        index += 20;                                  // Move index past "X-Firmware-Version: "
-        int endIndex = header.indexOf("\r\n", index); // End of the version line
+        index += 20;
+        int endIndex = header.indexOf("\r\n", index);
         if (endIndex == -1)
-            endIndex = header.length(); // If no newline, take till end
+            endIndex = header.length();
         newVersion = header.substring(index, endIndex);
     }
 
@@ -81,87 +76,69 @@ bool isFirmwareUpdateAvailable(const String &header, const String &currentVersio
     Serial.print("Available Firmware: ");
     Serial.println(newVersion);
 
-    // Compare version numbers (e.g., "1.2.1" vs "1.3.5")
-    if (newVersion > currentVersion)
-    {
-        Serial.println("Firmware update available!");
-        return true;
-    }
-    else
-    {
-        Serial.println("Firmware is up to date.");
-        return false;
-    }
+    return newVersion > currentVersion;
 }
 
 void ota_task(void *pv)
 {
-    // Initialize HTTPS
-    Serial.println("Initialize HTTPS");
-    modem.https_begin();
-
-    // Set OTA Server URL
-    if (!modem.https_set_url(server_url))
+    Serial.println("Connecting to GPRS...");
+    if (!modem.gprsConnect(APN, user, pass))
     {
-        Serial.println("Failed to set the URL. Please check the validity of the URL!");
-        modem.https_end();
+        Serial.println("Failed to connect to GPRS!");
         task_fatal_error();
     }
+    Serial.println("Connected to GPRS!");
 
-    // Send GET request
-    int httpCode = 0;
-    Serial.println("Get firmware form HTTPS");
-    httpCode = modem.https_get();
+    TinyGsmClient client(modem);
+    HttpClient http(client, server_url, server_port);
+
+    Serial.println("Sending GET request...");
+    http.get(firmware_path);
+
+    int httpCode = http.responseStatusCode();
     if (httpCode != 200)
     {
-        Serial.print("HTTP get failed ! error code = ");
+        Serial.print("HTTP GET failed! Error code = ");
         Serial.println(httpCode);
-        modem.https_end();
+        modem.gprsDisconnect();
         task_fatal_error();
     }
 
-    String headers = modem.https_header();
-    if (isFirmwareUpdateAvailable(headers, FIRMWARE) == false)
+    String headers = http.readString();
+    if (!isFirmwareUpdateAvailable(headers, FIRMWARE))
     {
-        modem.https_end();
+        modem.gprsDisconnect();
         task_fatal_error();
     }
 
-    // Serial.println("Headers :");
-    // Serial.println(headers);
-
-    // Get firmware size
-    size_t firmware_size = modem.https_get_size();
-    Serial.print("Firmware size : ");
+    size_t firmware_size = http.contentLength();
+    Serial.print("Firmware size: ");
     Serial.print(firmware_size / 1024);
-    Serial.println("Kb");
+    Serial.println(" KB");
 
-    // Begin Update firmware
-    Serial.println("Start upgrade firmware ...");
+    Serial.println("Starting firmware update...");
     if (!Update.begin(firmware_size))
     {
-        Serial.println("Not enough space to begin OTA");
-        modem.https_end();
+        Serial.println("Not enough space for OTA!");
+        modem.gprsDisconnect();
         task_fatal_error();
     }
 
     uint8_t buffer[1024];
-    int written = 0;
-    int progress = 0;
-    int total = 0;
+    int total = 0, progress = 0;
 
-    while (1)
+    while (total < firmware_size)
     {
-        // Read firmware form modem buffer
-        int len = modem.https_body(buffer, 1024);
-        if (len == 0)
+        int len = http.readBytes(buffer, sizeof(buffer));
+        if (len <= 0)
             break;
 
-        written = Update.write(buffer, len);
+        int written = Update.write(buffer, len);
         if (written != len)
         {
-            Serial.println("Written only : " + String(written) + "/" + String(len) + ". Retry?");
+            Serial.println("Write error! Retry?");
         }
+
         total += written;
         int newProgress = (total * 100) / firmware_size;
         if (newProgress - progress >= 5 || newProgress == 100)
@@ -170,26 +147,17 @@ void ota_task(void *pv)
             Serial.print(String("\r ") + progress + "%\n");
         }
     }
-    if (!Update.end())
+
+    if (!Update.end() || !Update.isFinished())
     {
-        Serial.println("Update not finished? Something went wrong!");
-        Serial.println("Error Occurred. Error #: " + String(Update.getError()));
-        modem.https_end();
+        Serial.println("Update failed!");
+        modem.gprsDisconnect();
         task_fatal_error();
     }
 
-    Serial.println();
-
-    if (!Update.isFinished())
-    {
-        Serial.println("Update successfully completed.");
-    }
-
-    Serial.println("=== Update successfully completed. Rebooting.");
-
+    Serial.println("Update completed! Rebooting...");
     delay(1500);
-
-    modem.https_end();
+    modem.gprsDisconnect();
     ota_task_handle = NULL;
     ESP.restart();
     vTaskDelete(NULL);
@@ -202,14 +170,14 @@ void IRAM_ATTR handleInterrupt()
 
 void setup()
 {
-    Serial.begin(115200); // Set console baud rate
-    Serial.println("Firmware version : " + String(FIRMWARE));
+    Serial.begin(115200);
+    Serial.println("Firmware version: " + String(FIRMWARE));
     SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
 
     int retry = 0;
     while (!modem.testAT(1000))
     {
-        Serial.println(".");
+        Serial.print(".");
         if (retry++ > 10)
         {
             delay(1000);
@@ -218,81 +186,27 @@ void setup()
     }
     Serial.println();
 
-    // Check if SIM card is online
-    SimStatus sim = SIM_ERROR;
-    while (sim != SIM_READY)
+    // Check SIM status
+    while (modem.getSimStatus() != SIM_READY)
     {
-        sim = modem.getSimStatus();
-        switch (sim)
-        {
-        case SIM_READY:
-            // Serial.println("SIM card online");
-            break;
-        case SIM_LOCKED:
-            Serial.println("The SIM card is locked. Please unlock the SIM card first.");
-            // const char *SIMCARD_PIN_CODE = "123456";
-            // modem.simUnlock(SIMCARD_PIN_CODE);
-            break;
-        default:
-            break;
-        }
+        Serial.println("Waiting for SIM...");
         delay(1000);
     }
 
-#ifdef APN
-    // Serial.printf("Set network apn : %s\n", APN);
+    // Configure APN
     modem.sendAT(GF("AT+QICSGP=1,1,\"" APN "\",\"" user "\",\"" pass "\",1"));
-    if (modem.waitResponse() != 1)
-    {
-        Serial.printf("Set network apn error : %s\n", APN);
-    }
-#endif
+    modem.waitResponse();
 
-    // Check network registration status and network signal status
-    int16_t sq;
-    Serial.println("Wait for the modem to register with the network.");
-    RegStatus status = REG_NO_RESULT;
-    while (status == REG_NO_RESULT || status == REG_SEARCHING || status == REG_UNREGISTERED)
+    // Network registration
+    Serial.println("Waiting for network...");
+    while (modem.getRegistrationStatus() <= REG_SEARCHING)
     {
-        status = modem.getRegistrationStatus();
-        switch (status)
-        {
-        case REG_UNREGISTERED:
-        case REG_SEARCHING:
-            sq = modem.getSignalQuality();
-            Serial.printf("[%lu] Signal Quality:%d\n", millis() / 1000, sq);
-            delay(1000);
-            break;
-        case REG_DENIED:
-            Serial.println("Network registration was rejected, please check if the APN is correct");
-            // return;
-            break;
-        case REG_OK_HOME:
-            Serial.println("Online registration successful");
-            break;
-        case REG_OK_ROAMING:
-            Serial.println("Network registration successful, currently in roaming mode");
-            break;
-        default:
-            Serial.printf("Registration Status:%d\n", status);
-            delay(1000);
-            break;
-        }
-    }
-    Serial.println();
-
-    String ueInfo;
-    if (modem.getSystemInformation(ueInfo))
-    {
-        Serial.print("Inquiring UE system information:");
-        Serial.println(ueInfo);
+        Serial.printf("Signal Quality: %d\n", modem.getSignalQuality());
+        delay(1000);
     }
 
-    delay(5000);
-
-    String ipAddress = modem.getLocalIP();
-    Serial.print("Network IP:");
-    Serial.println(ipAddress);
+    Serial.println("Network ready!");
+    Serial.println("IP Address: " + modem.getLocalIP());
 
     xTaskCreate(blink_task, "blink_task", 1024, NULL, 5, NULL);
     pinMode(23, INPUT_PULLUP);
@@ -301,12 +215,9 @@ void setup()
 
 void loop()
 {
-    if (otaBegin)
+    if (otaBegin && ota_task_handle == NULL)
     {
-        if (ota_task_handle == NULL)
-        {
-            xTaskCreatePinnedToCore(ota_task, "ota_task", 1024 * 8, NULL, 5, &ota_task_handle, 1);
-        }
+        xTaskCreatePinnedToCore(ota_task, "ota_task", 1024 * 8, NULL, 5, &ota_task_handle, 1);
         otaBegin = false;
     }
 }
